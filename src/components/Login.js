a86e54cf63407f2973a8b93166e22c180ea08cf9
YevaUsatova <yeva.usatova@gmail.com>
2021-08-11 23:11:41 -0400

    commit tree 602d8a8a5bc5daae569469eaef059388de57d6d2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-function Login({ setIsLoggedIn }) {
-  const history = useHistory();
+function Login({ onLogin}) {
+  const history= useHistory()
   const [formData, setFormData] = useState({
     username: "",
     password: "",
@@ -17,16 +17,25 @@ function Login({ setIsLoggedIn }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-
-    setIsLoggedIn(true);
+    fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+      })
+      .then((r) => r.json())
+      .then((user) => {
+        onLogin(user);
 
     // after logging the user in, redirect to the home page!
-    history.push("/");
+      history.push("/home");
+  });
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
+
       <input
         type="text"
         name="username"
